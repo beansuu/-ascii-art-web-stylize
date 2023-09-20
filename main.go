e32d33697d50a3ee328d817575d525c40eeab6e9
mtalli <mauno.talli@hotmail.com>
2023-09-20 18:45:29 +0300

    Website mobile friendly now

--- main.go
+++ main.go
@@ -49,6 +49,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	<html lang="en">
 	<head>
 		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>ASCII Art Web</title>
 		<link rel="stylesheet" href="/static/style.css">
 	</head>
